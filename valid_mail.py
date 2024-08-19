54ff6e3b90d9c14d1f5da3f11993023eba3ff781
Vladimir <milo-1.1@mail.ru>
2024-08-19 23:17:37 +0300

    Create valid_mail.py

---
+++ valid_mail.py
@@ -0,0 +1,8 @@
+def valid_mail(str):
+    return "@gmail.com" in str
+
+def test_1():
+    assert valid_mail("pytest@gmail.com")  # PASS
+
+def test_2():
+    assert valid_mail("pytest@gmail,com")  # FAILL
