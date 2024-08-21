7b540751db5d00f18e60b2c354cecbf17b3e137a
Vladimir <milo-1.1@mail.ru>
2024-08-21 20:22:41 +0300

    Add files via upload

---
+++ calculate_sum.py
@@ -0,0 +1,31 @@
+#Напишите функцию calculate_sum, которая принимает на вход одно целочисленное значение N и возвращает сумму всех чисел
+# от 1 до N включительно.
+
+def calculate_sum(N):
+    ordinal_numbers = []
+    if (N >= 0):
+        for i in range(1, N + 1):
+            ordinal_numbers.append(i)
+        return sum(ordinal_numbers)
+    else:
+        for i in range(abs(N) + 1):
+            ordinal_numbers.append(i)
+        return (-abs(sum(ordinal_numbers)) + 1)
+
+def test_calculate_sum_positive():
+    assert calculate_sum(11) == 66
+
+def test_calculate_sum_negative():
+    assert calculate_sum(-10) == (-54)
+
+def test_calculate_sum_zero():
+    assert calculate_sum(0) == 0
+
+
+"""
+N=11
+ordinal_numbers = []
+for i in range(N+1):
+    ordinal_numbers.append(i)
+print(sum(ordinal_numbers))
+"""
