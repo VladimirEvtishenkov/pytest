841c2a94affa10d43cda5030c597c46118789fa0
Vladimir <milo-1.1@mail.ru>
2024-08-23 10:35:57 +0300

    Add files via upload
    Доработка функции обработки ТС, добавление ТС

--- calculate_even_sum.py
+++ calculate_even_sum.py
@@ -2,7 +2,9 @@
 # списке.
 
 def calculate_even_sum(list_numbers):
-    return sum(numbers for numbers in list_numbers if numbers % 2 == 0)
+    def is_even(value):
+        return (type(value) is int or type(value) is float) and (value % 2 == 0)
+    return sum((number for number in list_numbers if is_even(number)))
 
 
 def test_positive_numbers():
@@ -13,7 +15,7 @@ def test_positive_numbers():
 def test_zero():
     list_numbers_tc = []
     assert calculate_even_sum(list_numbers_tc) == 0
-    list_numbers_tc = [0, 0, 0]
+    list_numbers_tc = [0, None, 0]
     assert calculate_even_sum(list_numbers_tc) == 0
 
 
@@ -26,3 +28,10 @@ def test_mix_numbers():
     list_numbers_tc = [1, -2, 4, 0, 2]
     assert calculate_even_sum(list_numbers_tc) == 4
 
+def test_char_and_numbers():
+    list_numbers_tc = [1, -2, 4, 'k', 2]
+    assert calculate_even_sum(list_numbers_tc) == 4
+
+def test_char_and_numbers():
+    list_numbers_tc = [1, -2, 4, 'k', 2, 0.1, 2.0]
+    assert calculate_even_sum(list_numbers_tc) == 6
